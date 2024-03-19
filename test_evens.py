a8393f73617f2e71d0a995a03cc2d9f95b31fdb2
Diego Cardenas <diego.cardenast@gmail.com>
2024-03-19 18:38:53 +0000

    feat: add two test statements

--- test_evens.py
+++ test_evens.py
@@ -2,9 +2,15 @@ import unittest
 from evens import even_number_of_evens
 
 class TestEvens(unittest.TestCase):
-    
-    def test_function_returns_true(self):
-        self.assertTrue(even_number_of_evens([]))
+
+    # def test_function_returns_true(self):
+    #     self.assertTrue(even_number_of_evens([]))
+
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
 
 
 if __name__ == '__main__':
