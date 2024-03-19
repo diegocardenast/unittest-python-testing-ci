7f7fab31aa124686a0ecb82f5b64b9af134e65af
Diego Cardenas <diego.cardenast@gmail.com>
2024-03-19 16:56:01 +0000

    feat: create main name conditionals

--- test_evens.py
+++ test_evens.py
@@ -1,7 +1,11 @@
 import unittest
+from evens import even_number_of_evens
 
 class TestEvens(unittest.TestCase):
-    pass
+    
+    def test_function_returns_true(self):
+        self.assertTrue(even_number_of_evens([]))
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
